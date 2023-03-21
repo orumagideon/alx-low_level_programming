30b7136de939d9b321f92ebc027fbeb3af511c16
orumagideon <orumagideon535@gmail.com>
2023-03-21 05:13:13 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "header_file_name.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet using _putchar
+ *
+ * Description: This function prints all the lowercase letters of the
+ * alphabet, from 'a' to 'z', using the _putchar function.
+ */
+
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}

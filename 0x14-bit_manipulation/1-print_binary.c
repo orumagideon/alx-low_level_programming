fe255984fd37b7f419b1dbac9f38740cf107ab71
orumagideon <orumagideon535@gmail.com>
2023-07-06 04:09:12 -0700

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - function prints the binary equivalent of a decimal number
+ * @n: printed binary number to be
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
